266cada0dc5a7c2991ee6ff6585e725dc55ff7d1
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-15 01:17:48 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
-
 if not game:IsLoaded() then
-            game.Loaded:Wait()
-        end
+        
+    game.Loaded:Wait()
+        
+end
+
+if game.PlaceId == 7234087065 then
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "script by nexer" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
 wait(1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(247.56419372558594, -265.0000305175781, -370.77752685546875)
@@ -27,3 +30,6 @@ wait(1)
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "teleporting",Text = "teleporting you back to slap battles!" ,Duration = 1000, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "OK"})
 wait(1)
 game:GetService("TeleportService"):Teleport(6403373529)
+end
+else
+game:GetService("TeleportService"):Teleport(7234087065)
